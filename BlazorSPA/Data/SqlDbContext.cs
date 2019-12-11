f207d5113cee4317ff97c30209ba8738f9c37258
Sarathlal Saseendran <Sarathlal.S@Orioninc.com>
2019-12-11 19:24:39 +0530

    Initial commit

---
+++ BlazorSPA/Data/SqlDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlazorSPA.Data
+{
+    public class SqlDbContext : DbContext
+    {
+        public SqlDbContext(DbContextOptions<SqlDbContext> options)
+           : base(options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# BlazorSPA
+ Repository for Blazor SPA with EF Core 3.1 Template
